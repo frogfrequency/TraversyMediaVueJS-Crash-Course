3a74fd11a2ef779e23776ca738185c50d2153a2a
frogfrequency <66001719+frogfrequency@users.noreply.github.com>
2022-08-05 22:03:42 +0200

    currently at minute 46:00 of the course

---
+++ vue-crash-2021/src/components/Button.vue
@@ -0,0 +1,26 @@
+ <template>
+  <button @click="myOnClick()" class="btn" :style="{ backgroundColor: color }">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "title-prop not passed to Button",
+    },
+    color: {
+      type: String,
+      default: "color-prop not passed to Button",
+    },
+  },
+  methods: {
+    myOnClick() {
+      console.log('trololololo')
+    }
+  }
+};
+</script>
